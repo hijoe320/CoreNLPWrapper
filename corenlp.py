27dda7ab7584cff73576f57b9ce0c3ececedaf72
H. Qu <hijoe320@gmail.com>
2015-11-24 09:17:15 +0000

    Update corenlp.py
    Removed __main__

--- corenlp.py
+++ corenlp.py
@@ -119,25 +119,3 @@ class CorenlpAnnotator(object):
                 token["begin"] = token.pop("CharacterOffsetBegin")
                 token["end"] = token.pop("CharacterOffsetEnd")
         return d
-        
-  
-if __name__ == "__main__":
-    # example params
-    params = {
-        "ner.model.7class": "ner_yahoo.ser.gz",
-        "annotators": "tokenize,ssplit,pos,lemma,ner"
-    }
-    ca = CorenlpAnnotator(
-        # path to stanford nlp
-        corenlp_classpath="/path/to/stanford-corenlp-full-2015-04-20",
-        memory_size="3g",
-        # for OSX EI Capitan installed with Oracle JDK
-        jvm_path="/Library/Java/JavaVirtualMachines/jdk1.8.0_66.jdk/Contents/Home/jre/lib/jli/libjli.dylib",
-    )
-    # will start an annotator
-    ca.run()
-    
-    print(ca.annotate("Hello World!"))
-    
-    # call this to stop the annotator
-    ca.stop()
